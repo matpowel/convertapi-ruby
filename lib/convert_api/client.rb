52805715f0ba5334454abef671cca66ccbfa550a
Matt Powell <mattonlife@icloud.com>
2020-09-10 18:38:57 -0500

    Use URI.encode_www_form_component instead of deprecated encode

--- lib/convert_api/client.rb
+++ lib/convert_api/client.rb
@@ -48,7 +48,7 @@ module ConvertApi
     def upload(io, filename)
       handle_response do
         request_uri = base_uri.path + 'upload'
-        encoded_filename = URI.encode(filename)
+        encoded_filename = URI.encode_www_form_component(filename)
 
         headers = DEFAULT_HEADERS.merge(
           'Content-Type' => 'application/octet-stream',
